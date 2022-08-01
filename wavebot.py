e83ceeaf0aaaf8f56469b7de828f48d8483708bd
fluffyteed <bshteddyboss@gmail.com>
2022-07-31 23:21:39 -0600

    .

--- wavebot.py
+++ wavebot.py
@@ -91,11 +91,13 @@ async def on_message(message):
             url = message.attachments[0].url
             filename = message.attachments[0].filename
 
-            r = requests.get(url, stream=True)
-            with open(filename, "wb") as out_file:
-                print("Saving .npy file:" + filename)
+            await message.attachments[0].save(fp="npyFiles/{}".format(filename))
 
-                shutil.copyfileobj(r.raw, out_file)
+            """r = requests.get(url, stream=True)
+            with open(filename, "wb") as out_file:
+                print("Saving .npy file: " + filename)
+
+                shutil.copyfileobj(r.raw, out_file)"""
 
     for word in bad_words:
         if message.content.count(word) > 0:
